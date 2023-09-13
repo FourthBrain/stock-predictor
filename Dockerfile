bf0d842f010722dec6b9e57a8fd3e4be5be59882
Flora Xu <flora.xushujing@gmail.com>
2023-09-12 17:42:34 -0700

    facelift

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,4 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -U -r requirements.txt
 
 COPY app/ .
-
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000" ]
--- app/model.py
+++ app/model.py
@@ -3,7 +3,6 @@ import datetime
 import pandas as pd
 
 import yfinance as yf
-
 from neuralprophet import NeuralProphet
 
 import argparse
@@ -11,24 +10,43 @@ from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
-TODAY = datetime.date.today()
+TOMORROW = datetime.date.today() + datetime.timedelta(days=1)
 
 def get_model_path(ticker:str):
+    #TODO: should we keep in tmp?
     return Path(BASE_DIR).joinpath(f"{ticker}.joblib")
 
 def get_data(ticker:str):
-    # Load the data from yf
-    data = yf.download(ticker, "2020-01-01", TODAY.strftime("%Y-%m-%d"))
+    """AI is creating summary for get_data
+
+    Args:
+        ticker (str): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    data = yf.download(ticker, "2020-01-01", TOMORROW)
     data.reset_index(inplace=True)
     data.rename(columns={"Date": "ds", "Adj Close": "y"}, inplace=True)
     data = data[["ds", "y"]]
     return data
 
 def save_model(model, ticker):
+    """AI is creating summary for save_model
+
+    Args:
+        model ([type]): [description]
+        ticker ([type]): [description]
+    """
     model.restore_trainer()  # avoid trainer being loaded into checkpoint 
     joblib.dump(model, get_model_path(ticker))
 
 def train(ticker:str="MSFT"):
+    """AI is creating summary for train
+
+    Args:
+        ticker (str, optional): [description]. Defaults to "MSFT".
+    """
     # Get history stock data
     data = get_data(ticker=ticker)
     # Initiate model and fit the model
@@ -39,26 +57,42 @@ def train(ticker:str="MSFT"):
 
 
 def predict(ticker="MSFT", days=7):
-    
+    """AI is creating summary for predict
+
+    Args:
+        ticker (str, optional): [description]. Defaults to "MSFT".
+        days (int, optional): [description]. Defaults to 7.
+
+    Returns:
+        [type]: [description]
+    """
     model_file = get_model_path(ticker)
     if not model_file.exists():
-        # return False
         train(ticker=ticker)
 
     model = joblib.load(model_file)
 
-    future_date = pd.bdate_range(start=TODAY, periods=days)[-1]
+    future_date = pd.bdate_range(start=TOMORROW, periods=days)[-1]
     dates = pd.bdate_range(start='2021-01-01', end=future_date.strftime("%Y-%m-%d"))
 
     future = pd.DataFrame({"ds": dates, "y": None})
     forecast = model.predict(future)
 
-    model.plot(forecast)
+    # TODO: plot should be allowed in streamlit app; currently it works in jupyter notebook
+    # model.plot(forecast)
     # model.plot_components(forecast).savefig(f"{ticker}_plot_components.png")
 
     return forecast.tail(days).to_dict("records")
 
-def convert(prediction_list):
+def convert(prediction_list) -> dict:
+    """AI is creating summary for convert
+
+    Args:
+        prediction_list ([type]): [description]
+        
+    Returns:
+        dict: [description]
+    """
     output = {}
     for data in prediction_list:
         date = data["ds"].strftime("%m/%d/%Y")
